d7556136f1f075f5f9556c28a78fb404be529bde
GoncaloMatias1 <up202108703@g.uporto.pt>
2023-11-30 17:47:35 +0000

    health class

--- src/main/java/NewGameStructure/Model/Game/Entities/Characteristics/Health.java
+++ src/main/java/NewGameStructure/Model/Game/Entities/Characteristics/Health.java
@@ -1,4 +1,34 @@
 package NewGameStructure.Model.Game.Entities.Characteristics;
 
 public class Health {
+    private int currentHealth;
+    private int maxHealth;
+
+    public Health(int maxHealth) {
+        this.maxHealth = maxHealth;
+        this.currentHealth = maxHealth; // A saúde começa no máximo por padrão.
+    }
+
+    public void takeDamage(int damage) {
+        currentHealth -= damage;
+        if (currentHealth < 0) {
+            currentHealth = 0; // A saúde não deve ficar negativa.
+        }
+    }
+
+    public void heal(int amount) {
+        currentHealth += amount;
+        if (currentHealth > maxHealth) {
+            currentHealth = maxHealth; // A saúde não deve exceder a saúde máxima.
+        }
+    }
+
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
+
+    public boolean isAlive() {
+        return currentHealth > 0;
+    }
+
 }
