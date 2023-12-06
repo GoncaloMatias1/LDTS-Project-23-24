4077f8ecbd77d9c160026343383e3f47e0175732
GonçaloMatias <up202108703@up.pt>
2023-12-06 23:40:51 +0000

    arena working

--- src/main/java/NewGameStructure/Controller/Menu/ArenaController.java
+++ src/main/java/NewGameStructure/Controller/Menu/ArenaController.java
@@ -1,26 +1,24 @@
 package NewGameStructure.Controller.Menu;
 
-
 import NewGameStructure.Application;
 import NewGameStructure.Controller.Controller;
 import NewGameStructure.GUI;
 import NewGameStructure.Model.Menu.ArenaModel;
-import NewGameStructure.Model.Menu.MainMenuModel;
-import com.googlecode.lanterna.input.KeyStroke;
-import com.googlecode.lanterna.input.KeyType;
 
 public class ArenaController extends Controller<ArenaModel> {
     public ArenaController(ArenaModel arenaModel){
         super(arenaModel);
     }
+
     @Override
     public void step(Application application, GUI.ACTION action){
         switch (action){
             case RIGHT:
                 getModel().movePlayerShipRight();
+                break; // Break statement added here
             case LEFT:
                 getModel().movePlayerShipLeft();
+                break; // Break statement added here
         }
     }
 }
-
--- src/main/java/NewGameStructure/View/Menu/ArenaView.java
+++ src/main/java/NewGameStructure/View/Menu/ArenaView.java
@@ -1,26 +1,38 @@
 package NewGameStructure.View.Menu;
 
-
 import NewGameStructure.GUI;
 import NewGameStructure.Model.Menu.ArenaModel;
-import NewGameStructure.Model.Menu.MainMenuModel;
 import NewGameStructure.View.View;
 import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.TextColor;
+
+import java.io.IOException;
 
 public class ArenaView extends View<ArenaModel> {
     public ArenaView(ArenaModel arenaModel){
         super(arenaModel);
     }
 
-
+    @Override
     public void draw(GUI gui) {
+        TextGraphics graphics = gui.getGraphics();
         gui.getScreen().clear();
-        gui.getGraphics().setForegroundColor(TextColor.ANSI.YELLOW);
-        // Desenhar limites usando model.getWidth() e model.getHeight()
-        // ...
-        // Desenhar playerShip usando model.getPlayerShip().draw(graphics)
-        // ...
+
+        // Draw the arena boundaries at the edges of the screen
+        graphics.setForegroundColor(TextColor.ANSI.YELLOW);
+        graphics.drawLine(0, 0, getModel().getWidth() - 1, 0, '-'); // Top boundary
+        graphics.drawLine(0, getModel().getHeight() - 1, getModel().getWidth() - 1, getModel().getHeight() - 1, '-'); // Bottom boundary
+        graphics.drawLine(0, 0, 0, getModel().getHeight() - 1, '|'); // Left boundary
+        graphics.drawLine(getModel().getWidth() - 1, 0, getModel().getWidth() - 1, getModel().getHeight() - 1, '|'); // Right boundary
+
+        // Draw the player ship
+        getModel().getPlayerShip().draw(graphics);
+
+        // Update the screen to show the latest drawings
+        try {
+            gui.getScreen().refresh();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
-
