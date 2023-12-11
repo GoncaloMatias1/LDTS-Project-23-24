9eb795c4407ea981aaff66a901c000ecd50c00bf
GoncaloMatias1 <up202108703@g.uporto.pt>
2023-12-11 15:03:54 +0000

    playership test correction

--- src/test/java/PlayerShipTest.java
+++ src/test/java/PlayerShipTest.java
@@ -1,4 +1,3 @@
-import NewGameStructure.Model.Game.Entities.Characteristics.Health;
 import NewGameStructure.Model.Game.Entities.PlayerShip;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,13 +7,12 @@ class PlayerShipTest {
 
     private PlayerShip playerShip;
     private final int screenWidth = 80; // Assume a screen width of 80 characters
-    private Health health;
+    private final int initialLives = 3; // Assume the player starts with 3 lives
 
     @BeforeEach
     void setUp() {
-        health = new Health(100);
         // Center the player ship horizontally on the screen
-        playerShip = new PlayerShip(screenWidth / 2, 20, screenWidth, health);
+        playerShip = new PlayerShip(screenWidth / 2, 20, screenWidth, initialLives);
     }
 
     @Test
@@ -35,5 +33,24 @@ class PlayerShipTest {
         }
         // Assert that the x position of the ship is at the right boundary
         assertEquals(screenWidth - 2, playerShip.getPosition().getX(), "Player ship should not move past right boundary");
+    }
+
+    @Test
+    void loseLife_ShouldDecreaseLivesByOne() {
+        int livesBefore = playerShip.getLives();
+        playerShip.loseLife();
+        assertEquals(livesBefore - 1, playerShip.getLives(), "Lose life should decrease lives by one");
+    }
+
+    @Test
+    void isAlive_WhenLivesAboveZero_ShouldReturnTrue() {
+        assertTrue(playerShip.isAlive(), "Player should be alive when lives are above zero");
+    }
+
+    @Test
+    void isAlive_WhenLivesZero_ShouldReturnFalse() {
+        // Set lives to zero
+        playerShip.setLives(0);
+        assertFalse(playerShip.isAlive(), "Player should not be alive when lives are zero");
     }
 }
