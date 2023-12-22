27300d8559b27c1fd433a5ad40448b4ede3dfd59
GonçaloMatias <up202108703@up.pt>
2023-12-22 19:54:42 +0000

    more tests

--- src/test/java/com/goncalomatias1/l05gr06/Controller/ControllerTest.java
+++ src/test/java/com/goncalomatias1/l05gr06/Controller/ControllerTest.java
@@ -1,6 +1,50 @@
 package com.goncalomatias1.l05gr06.Controller;
 
+import com.goncalomatias1.l05gr06.Application;
+import com.goncalomatias1.l05gr06.GUI;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class ControllerTest {
 
+    @Mock
+    private Application mockApplication;
+
+    private TestController testController;
+    private Object testModel;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        testModel = new Object();
+        testController = new TestController(testModel);
+    }
+
+    @Test
+    void testGetModel() {
+        assertEquals(testModel, testController.getModel());
+    }
+
+    @Test
+    void testStep() throws IOException {
+        GUI.ACTION testAction = GUI.ACTION.NONE;
+        testController.step(mockApplication, testAction);
+    }
+
+    private static class TestController extends Controller<Object> {
+
+        public TestController(Object model) {
+            super(model);
+        }
+
+        @Override
+        public void step(Application application, GUI.ACTION action) {
+        }
+    }
 }
--- src/test/java/com/goncalomatias1/l05gr06/States/Menu/ArenaStateTest.java
+++ src/test/java/com/goncalomatias1/l05gr06/States/Menu/ArenaStateTest.java
@@ -1,5 +1,19 @@
 package com.goncalomatias1.l05gr06.States.Menu;
 
-class ArenaStateTest {
+import com.goncalomatias1.l05gr06.Application;
+import com.goncalomatias1.l05gr06.Controller.Menu.ArenaController;
+import com.goncalomatias1.l05gr06.GUI;
+import com.goncalomatias1.l05gr06.Model.Menu.ArenaModel;
+import com.goncalomatias1.l05gr06.View.Menu.ArenaView;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import java.io.IOException;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArenaStateTest {
 }
